24ed15ded6db5fa5d8adb68a4cbc6c7835213aee
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2026-04-03 06:40:06 +0700

    v1.0.6

--- src/common/string/format/truncateString.ts
+++ src/common/string/format/truncateString.ts
@@ -1,14 +1,21 @@
+export type TruncateStringOptions = {
+  fromStart?: boolean
+}
+
 export function truncateString(
   str: string,
   maxLength: number | null | undefined,
+  options?: null | TruncateStringOptions,
 ): string
 export function truncateString(
   str: string | null | undefined,
   maxLength: number | null | undefined,
+  options?: null | TruncateStringOptions,
 ): string | null
 export function truncateString(
   str: string | null | undefined,
   maxLength: number | null | undefined,
+  options?: null | TruncateStringOptions,
 ): string | null {
   if (str == null) {
     return str ?? null
@@ -19,6 +26,9 @@ export function truncateString(
   if (str.length > maxLength) {
     if (maxLength <= 0) {
       return '…'
+    }
+    if (options?.fromStart) {
+      return '…' + str.slice(str.length - maxLength + 1)
     }
     return str.slice(0, maxLength - 1) + '…'
   }
