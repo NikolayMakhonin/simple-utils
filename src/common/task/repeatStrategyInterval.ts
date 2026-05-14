46d68b914f5c237901d0e7a8943591330edba729
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2026-05-14 14:57:40 +0700

    v2.3.5

--- src/common/task/repeatStrategyInterval.ts
+++ src/common/task/repeatStrategyInterval.ts
@@ -3,9 +3,7 @@ import type { TaskRepeatStrategy } from './types'
 export type RepeatStrategyIntervalOptions = {
   interval: number
   /**
-   * true - stop repeating if last execution failed
-   * false - do not stop repeating if last execution failed
-   * null/undefined - not set, does not overwrite previous strategies behavior
+   * stop repeating if last execution failed
    */
   stopIfFailed?: null | boolean
 }
@@ -21,13 +19,13 @@ export function repeatStrategyInterval({
     }
 
     return {
-      stop: stopIfFailed == null ? null : false,
+      stop: false,
       delay: status => {
         if (stopIfFailed && status.lastFailedRuns) {
           return { stop: true }
         }
         return {
-          stop: stopIfFailed == null ? null : false,
+          stop: false,
           delay: interval,
         }
       },
